7bedf831115f80c6bcd4c70cf5268ab7f3c85719
gauravsh0812 <>
2022-01-13 11:53:34 -0700

    update

--- test.py
+++ test.py
@@ -7,27 +7,27 @@ import torch
 #from preprocessing.preprocessing import preprocess
 
 def evaluate(model, iterator, criterion):
-    
+
     print('Let\'s start testing! ')
 
     model.eval()
-    
+
     epoch_loss = 0
-    
+
     with torch.no_grad():
-    
+
         for i, batch in enumerate(iterator):
 
             src = batch.latex
             trg = batch.mml
 
-            output = model(src, trg, 0) #turn off teacher forcing
+            output = model(src, trg, 0, False) #turn off teacher forcing
 
             #trg = [trg len, batch size]
             #output = [trg len, batch size, output dim]
 
             output_dim = output.shape[-1]
-            
+
             output = output[1:].view(-1, output_dim)
             trg = trg[1:].view(-1)
 
@@ -35,8 +35,7 @@ def evaluate(model, iterator, criterion):
             #output = [(trg len - 1) * batch size, output dim]
 
             loss = criterion(output, trg)
-            
-            epoch_loss += loss.item()
-        
-    return epoch_loss / len(iterator)
 
+            epoch_loss += loss.item()
+
+    return epoch_loss / len(iterator)
--- train.py
+++ train.py
@@ -3,40 +3,40 @@
 import torch
 
 def train(model, iterator, optimizer, criterion, clip):
-    
+
     print('Let\'s start training!')
 
     model.train()
-    
+
     epoch_loss = 0
-    
+
     for i, batch in enumerate(iterator):
-        
+
         src = batch.latex
         trg = batch.mml
         optimizer.zero_grad()
-        
-        output = model(src, trg)
-        
+
+        output = model(src, trg, 0.5, True)
+
         #trg = [trg len, batch size]
         #output = [trg len, batch size, output dim]
-        
+
         output_dim = output.shape[-1]
-        
+
         output = output[1:].view(-1, output_dim)
         trg = trg[1:].view(-1)
-        
+
         #trg = [(trg len - 1) * batch size]
         #output = [(trg len - 1) * batch size, output dim]
-        
+
         loss = criterion(output, trg)
-        
+
         loss.backward()
-        
+
         torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
-        
+
         optimizer.step()
-        
+
         epoch_loss += loss.item()
-        
+
     return epoch_loss / len(iterator)
